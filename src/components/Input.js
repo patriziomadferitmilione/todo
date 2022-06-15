35a7d416afbbd3be6458ed96fb4d7f657eafa6fe
Patrizio Milione <patriziomilione@gmail.com>
2022-06-15 11:23:54 +0200

    initial commit

---
+++ src/components/Input.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './Input.css';
+
+import { useDispatch } from 'react-redux';
+import { saveTodo } from '../features/todoSlice';
+
+const Input = () => {
+
+    const [input, setInput] = useState('');
+
+    const dispatch = useDispatch();
+
+    const addTodo = () => {
+      console.log(`yeyeye ${input}`)
+
+      dispatch(saveTodo({
+        item: input,
+        done: false,
+        id: Date.now()
+      }))
+
+      setInput('')
+    }
+
+  return (
+    <div className='input'>
+        <input type="text" value={input} onChange={e => setInput(e.target.value)} />
+        <button onClick={addTodo}>Add</button>
+    </div>
+  )
+}
+
+export default Input
